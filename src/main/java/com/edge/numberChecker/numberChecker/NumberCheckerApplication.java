e49b7a3c7ca433d32e86ec23d2c5f7927d8297f8
James O'Brien <jamesobrien442@gmail.com>
2022-01-06 23:29:17 +0000

    Initializing Project and adding technical test files in their original state

---
+++ src/main/java/com/edge/numberChecker/numberChecker/NumberCheckerApplication.java
@@ -0,0 +1,14 @@
+package com.edge.numberChecker.numberChecker;
+
+import com.edge.numberChecker.numberChecker.sampleFiles.CustomNumberEntity;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumberCheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edge/numberChecker/numberChecker/NumberCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edge.numberChecker.numberChecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumberCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
